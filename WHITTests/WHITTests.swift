7eba6aac6910067c038d899b7c1be1ce804c9bbf
m-lair <mlair@heidelberg.edu>
2024-11-27 16:11:25 -0600

    Initial Commit

---
+++ WHITTests/WHITTests.swift
@@ -0,0 +1,17 @@
+//
+//  WHITTests.swift
+//  WHITTests
+//
+//  Created by Marcus Lair on 11/27/24.
+//
+
+import Testing
+@testable import WHIT
+
+struct WHITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
